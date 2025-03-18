f669554acd76f70edca906a79cd830be1a5902c8
bravinkibetnyangusei <bravink599@gmail.com>
2025-03-18 06:36:19 +0300

    pages

---
+++ frontend/src/pages/Overview.js
@@ -0,0 +1,14 @@
+// src/pages/Overview.js
+import React from 'react';
+import '../styles/Admin.css';
+
+const Overview = () => {
+  return (
+    <div className="admin-main-content">
+      <h2>Overview</h2>
+      <p>Welcome to the Admin Overview page. Here you can monitor important statistics about your business.</p>
+    </div>
+  );
+};
+
+export default Overview;
